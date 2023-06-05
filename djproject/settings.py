5bfd923e6ed0eb83df7bbc4168b6c62a738e0b0b
Farzeena P A <farzinaayub266@gmail.com>
2023-06-05 22:32:58 +0530

    hostadded

--- djproject/settings.py
+++ djproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 # DEBUG=True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://djprojectweb.onrender.com/']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
